d37c1a0b64a2ca18e755e383334cd748fcba6bae
Tim Hsieh <timhsieh@rescomp-14-293347.stanford.edu>
2014-10-13 11:23:00 -0700

    Initial Commit

---
+++ Marguerite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Marguerite
+//
+//  Created by Tim Hsieh on 10/13/14.
+//  Copyright (c) 2014 Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
